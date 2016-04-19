dcc89720e7ee35214ff2072af5d6b5de2960bd15
Marco Chávez <totto_cha@hotmail.com>
2016-04-18 21:07:17 -0600

    Métodos de manejo para el control TabControl

---
+++ TabControl/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NuevoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AbrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.GuardarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.CerrarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CerrarTodoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TabControl = New System.Windows.Forms.TabControl()
+        Me.TabPagePrueba = New System.Windows.Forms.TabPage()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.TabControl.SuspendLayout()
+        Me.TabPagePrueba.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(484, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoToolStripMenuItem, Me.AbrirToolStripMenuItem, Me.ToolStripSeparator1, Me.GuardarToolStripMenuItem, Me.ToolStripSeparator2, Me.CerrarToolStripMenuItem, Me.CerrarTodoToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.ArchivoToolStripMenuItem.Text = "Archivo"
+        '
+        'NuevoToolStripMenuItem
+        '
+        Me.NuevoToolStripMenuItem.Name = "NuevoToolStripMenuItem"
+        Me.NuevoToolStripMenuItem.Size = New System.Drawing.Size(137, 22)
+        Me.NuevoToolStripMenuItem.Text = "Nuevo"
+        '
+        'AbrirToolStripMenuItem
+        '
+        Me.AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        Me.AbrirToolStripMenuItem.Size = New System.Drawing.Size(137, 22)
+        Me.AbrirToolStripMenuItem.Text = "Abrir"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(134, 6)
+        '
+        'GuardarToolStripMenuItem
+        '
+        Me.GuardarToolStripMenuItem.Name = "GuardarToolStripMenuItem"
+        Me.GuardarToolStripMenuItem.Size = New System.Drawing.Size(137, 22)
+        Me.GuardarToolStripMenuItem.Text = "Guardar"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(134, 6)
+        '
+        'CerrarToolStripMenuItem
+        '
+        Me.CerrarToolStripMenuItem.Name = "CerrarToolStripMenuItem"
+        Me.CerrarToolStripMenuItem.Size = New System.Drawing.Size(137, 22)
+        Me.CerrarToolStripMenuItem.Text = "Cerrar"
+        '
+        'CerrarTodoToolStripMenuItem
+        '
+        Me.CerrarTodoToolStripMenuItem.Name = "CerrarTodoToolStripMenuItem"
+        Me.CerrarTodoToolStripMenuItem.Size = New System.Drawing.Size(137, 22)
+        Me.CerrarTodoToolStripMenuItem.Text = "Cerrar Todo"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(137, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'TabControl
+        '
+        Me.TabControl.Controls.Add(Me.TabPagePrueba)
+        Me.TabControl.Location = New System.Drawing.Point(13, 28)
+        Me.TabControl.Name = "TabControl"
+        Me.TabControl.SelectedIndex = 0
+        Me.TabControl.Size = New System.Drawing.Size(459, 421)
+        Me.TabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed
+        Me.TabControl.TabIndex = 1
+        '
+        'TabPagePrueba
+        '
+        Me.TabPagePrueba.Controls.Add(Me.RichTextBox1)
+        Me.TabPagePrueba.Location = New System.Drawing.Point(4, 22)
+        Me.TabPagePrueba.Name = "TabPagePrueba"
+        Me.TabPagePrueba.Size = New System.Drawing.Size(451, 395)
+        Me.TabPagePrueba.TabIndex = 0
+        Me.TabPagePrueba.Text = "Pestaña Prueba"
+        Me.TabPagePrueba.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(451, 395)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 461)
+        Me.Controls.Add(Me.TabControl)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Manejar TabControl"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.TabControl.ResumeLayout(False)
+        Me.TabPagePrueba.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NuevoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AbrirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents GuardarToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents CerrarToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CerrarTodoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TabControl As System.Windows.Forms.TabControl
+    Friend WithEvents TabPagePrueba As System.Windows.Forms.TabPage
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ TabControl/Form1.vb
@@ -0,0 +1,95 @@
+﻿Public Class Form1
+    Dim contarTabPage As Integer = 0
+
+    Private Sub NuevoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NuevoToolStripMenuItem.Click
+        Dim textPage As String = "Pestaña " & (contarTabPage + 1)
+        Dim namePage As String = "Pestaña" & contarTabPage
+
+        addPage(textPage, namePage, "Puedes comenzar aquí...")
+    End Sub
+
+    Private Sub AbrirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AbrirToolStripMenuItem.Click
+        Dim OFD As New OpenFileDialog
+        OFD.Title = "Seleccione un archivo"
+        OFD.Filter = "Archivo TXT | *.txt"
+        OFD.ShowDialog()
+
+        Dim SR As New IO.StreamReader(OFD.FileName)
+        Dim namePage As String = "Pestaña" & contarTabPage
+        addPage(OFD.SafeFileName, namePage, SR.ReadToEnd)
+    End Sub
+    Private Sub addPage(textPage As String, namePage As String, texto As String)
+        Dim newPage As New TabPage()
+        newPage.Text = textPage
+        newPage.Name = namePage
+        TabControl.TabPages.Add(newPage)
+
+        addRchText(newPage, contarTabPage, texto)
+        contarTabPage += 1
+        TabControl.SelectedTab = newPage
+    End Sub
+
+    Private Sub addRchText(newPage As TabPage, contarTabPage As Integer, texto As String)
+        Dim newRchText As New RichTextBox
+        newRchText.Name = "RichTextBox" & contarTabPage
+        newRchText.Parent = newPage
+        newRchText.Text = texto
+        newRchText.Dock = DockStyle.Fill
+        newRchText.ForeColor = Color.Purple
+        newRchText.Visible = True
+
+        newPage.Controls.Add(newRchText)
+    End Sub
+
+    Private Sub GuardarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GuardarToolStripMenuItem.Click
+        Try
+            Dim childControl As Control
+            If (TabControl.SelectedTab.HasChildren) Then
+                For Each childControl In TabControl.SelectedTab.Controls
+                    If TypeOf childControl Is RichTextBox Then
+                        Try
+                            Dim SFD As New SaveFileDialog
+                            SFD.Title = "Selecciona el destino"
+                            SFD.Filter = "Archivo txt | *.txt"
+                            SFD.ShowDialog()
+
+                            Dim sw As New IO.StreamWriter(SFD.FileName)
+                            sw.Write(childControl.Text) 'COMPONENTE DEL QUE SE OBTENDRÁ EL TEXTO
+                            sw.Close()
+                        Catch ex As Exception
+                            MessageBox.Show("¡No se pudo guardar el archivo!")
+                        End Try
+                    End If
+                Next
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("¡No hay pestañas abiertas!", "Error")
+        End Try
+
+    End Sub
+
+    Private Sub CerrarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CerrarToolStripMenuItem.Click
+        Try
+            TabControl.TabPages.Remove(TabControl.SelectedTab)
+        Catch ex As Exception
+            MessageBox.Show("¡No hay pestañas abiertas!", "Error")
+        End Try
+    End Sub
+
+    Private Sub CerrarTodoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CerrarTodoToolStripMenuItem.Click
+        Try
+            TabControl.TabPages.Clear()
+        Catch ex As Exception
+            MessageBox.Show("¡No hay pestañas abiertas!", "Error")
+        End Try
+
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        End
+    End Sub
+
+
+
+End Class
